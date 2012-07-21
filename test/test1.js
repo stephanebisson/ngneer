cdda676d2c0bccd5039fb9af10fa63a017d083ae
Stephane Bisson <stephane.c.bisson@gmail.com>
2012-07-21 17:15:06 +1000

    commit a failing test to test the ci server and notification

--- test/test1.js
+++ test/test1.js
@@ -3,6 +3,6 @@ var ngneer = require('../src/lib/ngneer.js').ngneer;
 exports.testSomething = function(test){
     test.expect(2);
     test.ok(ngneer.ipa, "the ipa function is defined");
-    test.ok(ngneer.vanilla, "the vanilla function is defined");
+    test.ok(!ngneer.vanilla, "the vanilla function is defined");
     test.done();
 };
