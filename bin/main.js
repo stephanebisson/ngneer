949bf3d1688863029f447f8a2ad11cca0556a4e0
Stephane Bisson <stephane.c.bisson@gmail.com>
2012-07-21 14:28:31 +1000

    refactor the cli to use commander.js

--- bin/main.js
+++ bin/main.js
@@ -2,6 +2,18 @@
 
 var args = process.argv.slice(2);
 
-var ngneer = require('../lib/ngneer.js');
+// var ngneer = require('../lib/ngneer.js');
+// new ngneer.Ngneer(args).run();
 
-new ngneer.Ngneer(args).run();
+var program = require('commander');
+
+program.version('0.0.0');
+    
+program
+    .command('ipa')
+    .description('simplest angularjs single-page app')
+    .action(function(){
+        console.log('this is ipa');
+    });
+
+program.parse(process.argv);
