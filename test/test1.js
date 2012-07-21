3fa4ec77a5532b8b466ee1f1bc748428fa037fcf
Stephane Bisson <stephane.c.bisson@gmail.com>
2012-07-21 19:40:06 +1000

    renaming stuff

--- test/test1.js
+++ test/test1.js
@@ -2,7 +2,7 @@
 
 var ngneer = require('../src/lib/ngneer.js').ngneer;
 
-exports.testSomething = function (test) {
+exports.uselessTestNumber1 = function (test) {
     test.expect(2);
     test.ok(ngneer.ipa, "the ipa function is defined");
     test.ok(ngneer.vanilla, "the vanilla function is defined");
--- test/test2.coffee
+++ test/test2.coffee
@@ -2,7 +2,7 @@
 
 ngneer = require('../src/lib/ngneer.js').ngneer
 
-exports.testSomething =  (test) ->
+exports.uselessTestNumber2 =  (test) ->
     test.expect 2
     test.ok ngneer.ipa, "the ipa function is defined"
     test.ok ngneer.vanilla, "the vanilla function is defined"
