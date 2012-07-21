2547e6668843be2e134312d4a9f1100bc8da8d27
Stephane Bisson <stephane.c.bisson@gmail.com>
2012-07-21 14:30:11 +1000

    Revert "refactor the cli to use commander.js"
    This reverts commit 949bf3d1688863029f447f8a2ad11cca0556a4e0.

--- bin/main.js
+++ bin/main.js
@@ -2,18 +2,6 @@
 
 var args = process.argv.slice(2);
 
-// var ngneer = require('../lib/ngneer.js');
-// new ngneer.Ngneer(args).run();
+var ngneer = require('../lib/ngneer.js');
 
-var program = require('commander');
-
-program.version('0.0.0');
-    
-program
-    .command('ipa')
-    .description('simplest angularjs single-page app')
-    .action(function(){
-        console.log('this is ipa');
-    });
-
-program.parse(process.argv);
+new ngneer.Ngneer(args).run();
